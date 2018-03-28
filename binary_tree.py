40c31621c51ce83ab7b3219d9aa7fd1943e11850
Nicholas Gaydos <ngaydos@gmail.com>
2018-03-28 16:01:18 -0700

    binary_tree

---
+++ binary_tree.py
@@ -0,0 +1,30 @@
+#creates a binary tree that optimizes searching
+class Node(self, value):
+    def __init__:
+        self.value = value
+        self.left = None
+        self.right = None
+    def add(new):
+        if new > self.value:
+            if self.right is None:
+                self.right = Node(new)
+            else:
+                self.right.add(new)
+        if new < self.value:
+            if self.left is None:
+                self.left = Node(new)
+            else:
+                self.left.add(new)
+    def search(target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.search(target)
+        if target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.search(target)

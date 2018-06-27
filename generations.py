efdc1f0a390e9a6849d869ed4d1cf976ad9dedb0
Nicholas Gaydos <ngaydos@gmail.com>
2018-06-26 19:03:25 -0700

    updated generations script to add next generation creation

--- generations.py
+++ generations.py
@@ -17,13 +17,29 @@ def create_pairings(people_list):
     input = list of people (one generation)
     output = paired list of people
     '''
+    #added shuffle to the list so that if the initial created list is ordered it doesn't mess up the stats
     random.shuffle(people_list)
+    #creates an empty list to add to
     output_list = []
     for person1 in people_list:
         if person1.gender == 0 and person1.paired == False:
             for person2 in people_list:
                 if person2.gender == 1 and person2.paired == False:
-                    output_list.append({person1, person2})
+                    output_list.append([person1, person2])
                     person1.paired = True
                     person2.paired = True
     return output_list
+
+    #notes: consider adding a tracker for when the last person of gender 1 was used. It will allow you to run at less than n squared
+    #though it will add a bit of complexity up front.
+
+def next_generation(paired_list):
+    '''takes a list of paired lists and creates a number of children (between 0 and 3) each 
+    containing the last name of the first individual in the pair and with a randomly assigned gender
+    inputs: list of lists
+    outputs: list of person objects'''
+    output = []
+    for pair in paired_list:
+        for i in range(random.randint(0, 3)):
+            output.append(Person(pair[0].lastname, pair[0].generation + 1))
+    return output
